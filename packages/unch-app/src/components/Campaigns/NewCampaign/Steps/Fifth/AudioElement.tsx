d10f7cf2dcb8c1a00c3a893554c0efe3cf7ea0ac
Chris <chris.f@spinsci.com>
2019-10-16 15:48:55 -0500

    Basic design on Default Audio components

--- packages/unch-app/src/components/Campaigns/NewCampaign/Steps/Fifth/AudioElement.tsx
+++ packages/unch-app/src/components/Campaigns/NewCampaign/Steps/Fifth/AudioElement.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, Button} from 'antd'
+import {Card, Button, Descriptions, Icon, Input} from 'antd'
 
 export default class AudioElement extends React.Component<any, any>{
     constructor(props: any){
@@ -9,7 +9,14 @@ export default class AudioElement extends React.Component<any, any>{
     render(){
         return (
             <Card style={{width: '30vw', minWidth: 405}}>
-                Audio Element
+                <Descriptions column={1} layout="vertical" className="descriptionListColumn" title="">
+                    <Descriptions.Item label="Default Audio:">
+                        <Icon style={{fontSize: '32px', color: '#1890ff'}} type="play-circle" />
+                    </Descriptions.Item>
+                    <Descriptions.Item label="TTS:">
+                        <Input.TextArea placeholder="Hello this is so and so"/>
+                    </Descriptions.Item>
+                </Descriptions>
                 <Button icon="delete" onClick={() => this.props.delete(this.props.index)}></Button>
             </Card>
         )
--- packages/unch-app/src/components/Campaigns/NewCampaign/Steps/Fifth/AudioOptionsElement.tsx
+++ packages/unch-app/src/components/Campaigns/NewCampaign/Steps/Fifth/AudioOptionsElement.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, Button} from 'antd'
+import {Card, Button, Descriptions, Input, Icon} from 'antd'
 
 
 export default class AudioOptionsElement extends React.Component<any, any>{
@@ -10,7 +10,15 @@ export default class AudioOptionsElement extends React.Component<any, any>{
     render(){
         return (
             <Card style={{width: '30vw', minWidth: 405}}>
-                Audio with Options
+                <Descriptions column={1} layout="vertical" className="descriptionListColumn" title="">
+                    <Descriptions.Item label="Default Audio:">
+                        <Icon type="play-circle" />
+                    </Descriptions.Item>
+                    <Descriptions.Item label="TTS:">
+                        <Input.TextArea placeholder="Hello this is so and so"/>
+                    </Descriptions.Item>
+                </Descriptions>
+                Audio WITH Options Element
                 <Button icon="delete" onClick={() => this.props.delete(this.props.index)}></Button>
             </Card>
         )
